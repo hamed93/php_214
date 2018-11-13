33ebe71a4ba0e1c1fb8fff1c550b21dd1a8d783c
hamed93 <hfarahani185@gmail.com>
2018-11-13 19:10:15 +0330

    create database table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\PanelController;
+
+
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +17,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//
+Route::get('/admin/panel','Admin/PanelController@index');
+//Route::resourse('')
+
